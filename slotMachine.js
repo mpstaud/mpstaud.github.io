1f29a45a90edd513dc68289447d2b77b7204a291
Matt Staudacher <98075113+mpstaud@users.noreply.github.com>
2023-08-25 21:26:50 -0500

    .

--- slotMachine.js
+++ slotMachine.js
@@ -5,17 +5,16 @@ let num4;
 let num5;
 let num6;
 
-function spin(){
-    num1 = Math.random() * 5;
-    let yourNumber = num1;
-    return yourNumber;
-  
+function spin() {
+  num1 = Math.random() * 5;
+  let yourNumber = num1;
+  return yourNumber;
 }
 
-function match(){
-    num4 = Math.random() * 5;
-    let luckyNumber = num4;
-    return luckyNumber;
+function match() {
+  num4 = Math.random() * 5;
+  let luckyNumber = num4;
+  return luckyNumber;
 }
 
 let att = spin();
